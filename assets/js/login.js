df56bc5c0810c90eecc2b801668cd0caea248606
Niharikasahu007 <niharika.sahu.work@gmail.com>
2025-06-18 17:30:53 +0530

    Updated validation files

---
+++ assets/js/login.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){    
+    $("#loginForm").validate({
+        rules:{
+            email:{
+                required:true,
+                email:true
+            },
+            pass:{
+                required:true,
+                minlength:8
+            }
+        },
+        messages:{
+            email:{
+                required:"Email is Required",
+                email:"Enter a valid Email adress"
+            },
+            pass:{
+                required:"Password is Required",
+                minlength:"Password must be at least 8 characters long"
+            }
+        }
+    });
+});
