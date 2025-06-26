bcf75fafd883f7a58a9128611a824a1245dd83d3
Latha Vangimalli <lathavangimalli@gmail.com>
2025-06-26 16:36:56 +0530

    Update Reservation.java

--- java backend/src/model/Reservation.java
+++ java backend/src/model/Reservation.java
@@ -2,42 +2,14 @@ package model;
 
 import java.time.LocalDate;
 
-public class Reservation {
-    private final String reservationId;
-    private final User user;
-    private final Book book;
-    private final LocalDate reservationDate;
-
-    public Reservation(String reservationId, User user, Book book, LocalDate reservationDate) {
-        this.reservationId = reservationId;
-        this.user = user;
-        this.book = book;
-        this.reservationDate = reservationDate;
-    }
-
-    public String getReservationId() {
-        return reservationId;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public Book getBook() {
-        return book;
-    }
-
-    public LocalDate getReservationDate() {
-        return reservationDate;
-    }
-
+public record Reservation(String reservationId, User user, Book book, LocalDate reservationDate) {
     @Override
     public String toString() {
-        return "Reservation {" +
-                "ID='" + reservationId + '\'' +
-                ", User='" + user.getFullName() + '\'' +
-                ", Book='" + book.getTitle() + '\'' +
-                ", Date=" + reservationDate +
+        return "Reservation{" +
+                "reservationId='" + reservationId + '\'' +
+                ", user=" + user +
+                ", book=" + book +
+                ", reservationDate=" + reservationDate +
                 '}';
     }
 }
