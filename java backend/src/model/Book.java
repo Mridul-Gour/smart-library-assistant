40b0f01c95583c1df157e2705bccb1ecb34d9a6d
Latha Vangimalli <lathavangimalli@gmail.com>
2025-06-26 16:36:27 +0530

    Update Book.java

--- java backend/src/model/Book.java
+++ java backend/src/model/Book.java
@@ -1,45 +1,13 @@
 package model;
 
-public class Book {
-    private final String bookId;
-    private final String title;
-    private final String author;
-    private boolean isAvailable;
-
-    public Book(String bookId, String title, String author, boolean isAvailable) {
-        this.bookId = bookId;
-        this.title = title;
-        this.author = author;
-        this.isAvailable = isAvailable;
-    }
-
-    public String getBookId() {
-        return bookId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public boolean isAvailable() {
-        return isAvailable;
-    }
-
-    public void setAvailable(boolean available) {
-        this.isAvailable = available;
-    }
-
+public record Book(String bookId, String title, String author, boolean available) {
     @Override
     public String toString() {
-        return "Book { " +
-               "bookId='" + bookId + '\'' +
-               ", title='" + title + '\'' +
-               ", author='" + author + '\'' +
-               ", available=" + isAvailable +
-               " }";
+        return "Book{" +
+                "bookId='" + bookId + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", available=" + available +
+                '}';
     }
 }
