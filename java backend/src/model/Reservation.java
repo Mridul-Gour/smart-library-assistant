e9a33a9cd18ca2141b92aa6a77e33d907b6054ec
Latha Vangimalli <lathavangimalli@gmail.com>
2025-06-26 18:36:01 +0530

    Update Reservation.java

--- java backend/src/model/Reservation.java
+++ java backend/src/model/Reservation.java
@@ -2,14 +2,42 @@ package model;
 
 import java.time.LocalDate;
 
-public record Reservation(String reservationId, User user, Book book, LocalDate reservationDate) {
-    @Override
-    public String toString() {
-        return "Reservation{" +
-                "reservationId='" + reservationId + '\'' +
-                ", user=" + user +
-                ", book=" + book +
-                ", reservationDate=" + reservationDate +
-                '}';
+public sealed interface Reservation permits Reservation.NormalReservation, Reservation.UrgentReservation {
+    Book getBook();
+    User getUser();
+    LocalDate getReservationDate();
+
+    record NormalReservation(Book book, User user, LocalDate reservationDate) implements Reservation {
+        @Override
+        public Book getBook() {
+            return book;
+        }
+
+        @Override
+        public User getUser() {
+            return user;
+        }
+
+        @Override
+        public LocalDate getReservationDate() {
+            return reservationDate;
+        }
+    }
+
+    record UrgentReservation(Book book, User user, LocalDate reservationDate, String reason) implements Reservation {
+        @Override
+        public Book getBook() {
+            return book;
+        }
+
+        @Override
+        public User getUser() {
+            return user;
+        }
+
+        @Override
+        public LocalDate getReservationDate() {
+            return reservationDate;
+        }
     }
 }
